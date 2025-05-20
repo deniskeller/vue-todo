9d3c3687fe574e7756156b847d0304d1a44e5930
deniskeller <dentean@mail.ru>
2025-05-21 00:22:54 +0400

    fix deploy problem

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,13 @@
 /// <reference types="vite/client" />
+
+// Пример объявления для .vue файлов
+declare module '*.vue' {
+  import type { DefineComponent } from 'vue';
+  const component: DefineComponent<{}, {}, any>;
+  export default component;
+}
+
+// Для других типов файлов
+declare module '*.svg';
+declare module '*.png';
+declare module '*.jpg';
