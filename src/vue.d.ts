3837939dfffb591c83cb7e8d9d6b63d0a0df7ee0
deniskeller <dentean@mail.ru>
2025-05-21 00:28:18 +0400

    fix prob;llem

---
+++ src/vue.d.ts
@@ -0,0 +1,13 @@
+declare module '*.vue' {
+  import type { DefineComponent } from 'vue';
+  const component: DefineComponent<{}, {}, any>;
+  export default component;
+}
+
+// Добавьте для алиасов
+declare module '@components/*' {
+  import type { DefineComponent }
+  import type { DefineComponent } from 'vue';
+  const component: DefineComponent<{}, {}, any>;
+  export default component;
+}
