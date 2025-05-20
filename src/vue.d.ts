60d4640e3064db5503b94a678608388124b50aa7
deniskeller <dentean@mail.ru>
2025-05-21 00:31:24 +0400

    fix prob;llem

--- src/vue.d.ts
+++ src/vue.d.ts
@@ -5,9 +5,9 @@ declare module '*.vue' {
 }
 
 // Добавьте для алиасов
-declare module '@components/*' {
-  import type { DefineComponent }
-  import type { DefineComponent } from 'vue';
-  const component: DefineComponent<{}, {}, any>;
-  export default component;
-}
+// declare module '@components/*' {
+//   import type { DefineComponent }
+//   import type { DefineComponent } from 'vue';
+//   const component: DefineComponent<{}, {}, any>;
+//   export default component;
+// }
