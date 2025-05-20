c836c06cddadf392ea56455b75b7b760a6ae52cc
deniskeller <dentean@mail.ru>
2025-05-21 00:34:24 +0400

    fix problem

--- src/vue.d.ts
+++ src/vue.d.ts
@@ -1,6 +1,8 @@
+/// <reference types="vite/client" />
+
 declare module '*.vue' {
-  import type { DefineComponent } from 'vue';
-  const component: DefineComponent<{}, {}, any>;
+  import type { ComponentOptions } from 'vue';
+  const component: ComponentOptions;
   export default component;
 }
 
